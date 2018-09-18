ddbc8011091dc701c3934adc9c1865bbe1bbb619
Janith Leanage <janithleanage@gmail.com>
2018-09-18 18:38:36 +0530

    back to the start

--- domain/publications.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-// Publications exposes the repository interface expected by the business domain
-type Publications interface {
-	CreatePublication(*Publication) int
-	RetrievePublication(int) *Publication
-	UpdatePublication(*Publication) bool
-	DeletePublication(int) bool
-}
--- paataka.go
+++ paataka.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/janithl/paataka/database"
-	"github.com/janithl/paataka/domain"
-	"github.com/janithl/paataka/web"
 )
 
 func main() {
 	fmt.Println("Paataka v1.0")
-
-	pd := database.NewSQLPublicationsRepository("sql")
-	ps := domain.NewPublicationServiceImpl(pd)
-	pc := web.NewPublicationController(ps)
-
-	http.Handle("/publications/", pc)
-	log.Fatal(http.ListenAndServe("localhost:8067", nil))
 }
