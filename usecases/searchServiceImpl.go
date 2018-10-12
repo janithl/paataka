329554147a8f284c4b79c8f237faa587e3aecc60
Janith Leanage <janithleanage@gmail.com>
2018-10-13 01:57:44 +0530

    implemented search service to pass tests

--- usecases/searchServiceImpl.go
+++ usecases/searchServiceImpl.go
@@ -1,23 +1,49 @@
 package usecases
 
+import (
+	"regexp"
+	"strings"
+)
+
 // SearchServiceImpl is an implementation of the SearchService
 type SearchServiceImpl struct {
-	searchIndex []SearchObject
+	searchIndex map[string][]SearchObject
+	wordRegexp  *regexp.Regexp
 }
 
 // NewSearchServiceImpl returns a new instance of SearchServiceImpl
 func NewSearchServiceImpl() *SearchServiceImpl {
 	return &SearchServiceImpl{
-		searchIndex: make([]SearchObject, 0),
+		searchIndex: make(map[string][]SearchObject),
+		wordRegexp:  regexp.MustCompile(`\w+`),
 	}
 }
 
 // Index adds the given object to the search index
 func (s *SearchServiceImpl) Index(obj SearchObject) {
-	s.searchIndex = append(s.searchIndex, obj)
+	matches := s.wordRegexp.FindAllString(strings.ToLower(obj.Content), -1)
+
+	for _, match := range matches {
+		s.searchIndex[match] = append(s.searchIndex[match], obj)
+	}
 }
 
 // Search returns the list of relevant results
 func (s *SearchServiceImpl) Search(objtype string, query string) ([]SearchObject, error) {
-	return []SearchObject{}, nil
+	matches := s.wordRegexp.FindAllString(strings.ToLower(query), -1)
+
+	results := make([]SearchObject, 0)
+	for _, match := range matches {
+		for _, objmatch := range s.searchIndex[match] {
+			if objmatch.Type == objtype {
+				results = append(results, objmatch)
+			}
+		}
+	}
+
+	if len(results) == 0 {
+		return results, ErrNoSearchResults
+	}
+
+	return results, nil
 }
